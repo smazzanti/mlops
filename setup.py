8dfe4808745a1d9dc166481ced85a5181b7ec724
smazzanti <41154179+smazzanti@users.noreply.github.com>
2021-09-19 23:11:29 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+  name='mlops',
+  version='0.1',
+  description='MLOps',
+  url='https://github.com/smazzanti/mlops',
+  author='Samuele Mazzanti',
+  author_email='mazzanti.sam@gmail.com',
+  license='GNU',
+  packages=['mlops'],
+  install_requires=[
+    'numpy>=1.18.1'
+  ],
+  zip_safe=False
+)
